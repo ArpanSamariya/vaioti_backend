6732fe8990d3cdf1f6600baab615b14cf838dee4
sanjh38 <sanjh38@hotmail.com>
2017-06-22 11:36:33 +0530

    first commit

---
+++ backend/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var URL="mongodb://nodemon:123456@ds131782.mlab.com:31782/vaioti";
+
+const db=require('monk')(URL);
+
+const docs=db.get('pro');
+/* GET home page. */
+router.get('/welcome', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+
+    docs.insert({first_name:"Prabhas"},function (err,docs){
+        if(err) console.log(err);
+        else res.json(docs);
+    })
+});
+router.get('/wel', function(req, res, next) {
+    res.send('Sahi Chal rha hai!');
+});
+
+module.exports = router;
